a7fc1016cd98d2e3278a7e9f8841fa5aa53f8f5f
NimaQu <i@nimaqu.com>
2022-01-31 05:11:04 -0500

    添加群组自定义正则功能，群组配置文件功能（未完成）

--- dbhelper.py
+++ dbhelper.py
@@ -8,13 +8,38 @@ class DBHelper:
         self.conn = sqlite3.connect(dbname)
 
     def setup(self):
-        stmt = ('''CREATE TABLE IF NOT EXISTS user
-       (user_id int PRIMARY KEY     NOT NULL,
-        last_try int default 0,
-        blacklist  int  default 1 NOT NULL,
-        try_count int default 1);''')
+        stmt = ('''
+        CREATE TABLE IF NOT EXISTS user
+        (
+        user_id INTEGER PRIMARY KEY NOT NULL,
+        last_try INTEGER default 0,
+        blacklist  INTEGER  default 1 NOT NULL,
+        try_count INTEGER default 1
+        );
+        
+        CREATE TABLE IF NOT EXISTS group_config
+        (
+        group_id INTEGER PRIMARY KEY NOT NULL,
+        captcha_failed_action TEXT,
+        captcha_timeout_action TEXT,
+        captcha_timeout INTEGER,
+        enable_global_blacklist INTEGER,
+        enable_regex INTEGER,
+        enable_third_party_blacklist INTEGER
+        );
+        
+        CREATE TABLE IF NOT EXISTS regex
+        (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NULL,
+        group_id INTEGER NOT NULL,
+        regex BLOB NOT NULL,
+        match TEXT NOT NULL,
+        action TEXT NOT NULL,
+        description TEXT NOT NULL
+        );
+        ''')
         try:
-            self.conn.execute(stmt)
+            self.conn.executescript(stmt)
             self.conn.commit()
         except sqlite3.Error as e:
             logging.error(str(e))
@@ -125,6 +150,90 @@ class DBHelper:
         args = user_id
         try:
             self.conn.executemany(stmt, args)
+            self.conn.commit()
+        except sqlite3.Error as e:
+            logging.error(str(e))
+
+    def new_regex(self, group_id, regex, match, action, description):
+        if self.regex_count(group_id) >= 10:
+            return False
+        stmt = "INSERT OR REPLACE INTO regex (group_id, regex, match, action, description) VALUES (?,?,?,?,?)"
+        args = (group_id, regex, match, action, description)
+        try:
+            self.conn.execute(stmt, args)
+            self.conn.commit()
+        except sqlite3.Error as e:
+            logging.error(str(e))
+        return True
+
+    def get_regex(self, group_id):
+        stmt = "SELECT * FROM regex WHERE group_id == (?)"
+        args = group_id
+        cur = self.conn.cursor()
+        try:
+            cur.execute(stmt, (args,))
+            result = cur.fetchall()
+        except sqlite3.Error as e:
+            logging.error(str(e))
+            return None
+        return result
+
+    def regex_count(self, group_id):
+        stmt = "SELECT COUNT(*) FROM regex WHERE group_id == (?)"
+        args = group_id
+        cur = self.conn.cursor()
+        try:
+            cur.execute(stmt, (args,))
+            result = cur.fetchone()[0]
+        except sqlite3.Error as e:
+            logging.error(str(e))
+            return None
+        return result
+
+    def delete_regex(self, rid, group_id):
+        stmt = "DELETE FROM regex WHERE id = (?) and group_id = (?)"
+        args = (rid, group_id)
+        cur = self.conn.cursor()
+        try:
+            cur.execute(stmt, args)
+            rows = cur.rowcount
+            self.conn.commit()
+            if rows == 0:
+                return False
+            else:
+                return True
+        except sqlite3.Error as e:
+            logging.error(str(e))
+            return None
+
+    def get_group_config(self, group_id):
+        stmt = "SELECT * FROM group_config WHERE group_id == (?)"
+        args = group_id
+        cur = self.conn.cursor()
+        try:
+            cur.execute(stmt, (args,))
+            result = cur.fetchone()
+        except sqlite3.Error as e:
+            logging.error(str(e))
+            return None
+
+    def new_group_config(self, group_id):
+        stmt = "INSERT OR REPLACE INTO group_config (group_id) VALUES (?)"
+        args = (group_id,)
+        try:
+            self.conn.execute(stmt, (args,))
+            self.conn.commit()
+        except sqlite3.Error as e:
+            logging.error(str(e))
+            return None
+
+    def set_group_config(self, group_id, key, value):
+        if self.get_group_config(group_id) is None:
+            self.new_group_config(group_id)
+        stmt = "UPDATE group_config SET " + key + " = ? WHERE group_id == ?"
+        args = (value, group_id)
+        try:
+            self.conn.execute(stmt, (args,))
             self.conn.commit()
         except sqlite3.Error as e:
             logging.error(str(e))
