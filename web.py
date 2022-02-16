e70e37e16925b782ca952f40eeec9eac3e0286dd
NimaQu <i@nimaqu.com>
2022-02-15 20:48:27 -0500

    更新 html 模板和  readme

--- web.py
+++ web.py
@@ -16,41 +16,46 @@ _channel = 0
 
 
 @web.route("/")
-def hello_world():
-    if not client.is_connected:
-        return '<h1>Not connected</h1>'
-    return str(client.get_me())
+def root():
+    return render_template('index.html')
 
 
 @web.route("/recaptcha", methods=["GET", "POST"])
 async def verify():
+    # 单元测试-----------------------------------------------------
+    # return render_template('recaptcha.html', sitekey=114514)
+    # 单元测试-----------------------------------------------------
+
     if request.args.get('challenge') is None:
-        return '<h1>Welcome pages</h1>'
+        flash("没有这条验证数据！", "error")
+        return render_template('result.html')
     else:
         challenge_id = request.args.get('challenge')
-
     try:
         ch_id, challenge_data = _current_challenges.get_by_challenge_id(challenge_id)
     except TypeError:
-        return '<h1>Challenge not found</h1>'
+        flash("没有这条验证数据！", "error")
+        return render_template('result.html')
 
     if challenge_data is None or ch_id is None:
-        return '<h1>Challenge not found</h1>'
-    challenge, target_id, timeout_event = challenge_data
+        flash("没有这条验证数据！", "error")
+        return render_template('result.html')
 
+    challenge, target_id, timeout_event = challenge_data
     chat_id = challenge.message.chat.id
     msg_id = challenge.message.message_id
 
     if chat_id != challenge.message.chat.id:
-        return '<h1>Unknown error</h1>'
+        flash("未知错误！", "error")
+        return render_template('result.html')
     group_config = _config.get(str(chat_id), _config["*"])
     chat_title = challenge.message.chat.title
 
     if request.method == "POST":
         captcha_response = request.form['g-recaptcha-response']
         if not challenge.verify(captcha_response):
-            flash('Invalid reCAPTCHA status. Please try again.')
-            return '<h1>Invalid reCAPTCHA status. Please try again.</h1>'
+            flash('验证状态异常，请再试一次！', 'error')
+            return render_template('recaptcha.html', sitekey=challenge.site_key)
         else:
             timeout_event.stop()
             try:
@@ -96,10 +101,14 @@ async def verify():
                 except MessageNotModified as e:
                     await client.send_message(int(_channel),
                                               'Bot 运行时发生异常: `' + str(e) + "`")
-            return '<h1>Success</h1>'
+            flash('您已通过验证，欢迎加入本群！如果仍然无法发言，请重启 Telegram 客户端。')
+            return render_template('result.html')
     else:
         return render_template('recaptcha.html', sitekey=challenge.site_key)
 
 
 if __name__ == '__main__':
+    web.debug = True
+    web.env = 'debug'
+    web.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     web.run(port=8181, host="127.0.0.1")
